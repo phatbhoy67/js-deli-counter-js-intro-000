391e2fe78192cfc8c8a740083ce9785f94e6e680
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-10 12:42:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(array, name){
   array.push(name);
   return `Welcome, ${name}. You are number ${array.length} in line.`;
 }
+
+function nowServing(array) {
+  if(array.length > 0) {
+    return `Currently serving ${array[0]}`;
+    array.unshift();
+  } else {
+    return `There is nobody waiting to be served!`;
+  }
+}
